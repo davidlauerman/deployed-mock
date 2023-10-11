a5b009c3c2a6e1900924cd90c51383105d2d3e2e
davidlauerman <david_lauerman@brown.edu>
2023-10-12 00:55:39 +0200

    feat: implemented file path capability

--- mock/src/components/mockedJson.tsx
+++ mock/src/components/mockedJson.tsx
@@ -1,32 +1,33 @@
 import "../styles/main.css";
-import { useState, useEffect } from "react";
 
-// Function to return a 2D array of integers
-function getIntegers(): number[][] {
-  return [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
+export function getData(file: string): string[][] {
+  if (file === "fileInt") {
+    // If input matches 'string1', return the first predetermined array.
+    return [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"],
+    ];
+  } else if (file === "fileTxt") {
+    // If input matches 'string2', return the second predetermined array.
+    return [
+      ["Apple", "Banana", "Cherry"],
+      ["Dog", "Elephant", "Fox"],
+      ["A", "B", "C"],
+      ["1", "2", "34"],
+      ["lorem", "ipsum, dolor", "sit, amit!"],
+    ];
+  } else if (file === "fileRnd") {
+    // If input matches 'string3', return the third predetermined array.
+    return getRandomArray(5, 5);
+  } else {
+    // If input doesn't match any of the predetermined strings, return null or perform another action.
+    return [["Wrong"]];
+  }
 }
 
-// Function to return a 2D array of strings
-function getStrings(): string[][] {
-  return [
-    ["Apple", "Banana", "Cherry"],
-    ["Dog", "Elephant", "Fox"],
-    ["A", "B", "C"],
-    ["1", "2", "34"],
-    ["lorem", "ipsum, dolor", "sit, amit!"],
-  ];
-}
-
-// Function to generate a random 2D array of strings
-function getRandomArray(
-  rows: number,
-  cols: number,
-  stringLength: number
-): string[][] {
+// // Function to generate a random 2D array of strings
+function getRandomArray(rows: number, cols: number): string[][] {
   const randomArray: string[][] = [];
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -35,7 +36,7 @@ function getRandomArray(
     const row: string[] = [];
     for (let j = 0; j < cols; j++) {
       let randomString = "";
-      for (let k = 0; k < stringLength; k++) {
+      for (let k = 0; k < 1 * Math.random(); k++) {
         const randomCharIndex = Math.floor(Math.random() * characters.length);
         randomString += characters.charAt(randomCharIndex);
       }
@@ -46,14 +47,34 @@ function getRandomArray(
   return randomArray;
 }
 
-export { getIntegers, getStrings, getRandomArray };
+// // Function to return a 2D array of integers
+// function getIntegers(): number[][] {
+//   return [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ];
+// }
+
+// // Function to return a 2D array of strings
+// function getStrings(): string[][] {
+//   return [
+//     ["Apple", "Banana", "Cherry"],
+//     ["Dog", "Elephant", "Fox"],
+//     ["A", "B", "C"],
+//     ["1", "2", "34"],
+//     ["lorem", "ipsum, dolor", "sit, amit!"],
+//   ];
+// }
+
+// export { getIntegers, getStrings, getRandomArray };
 
 // export function DataLoader({ filePath }) {
 //   const [data, setData] = useState(null);
 //   useEffect(() => {
 //     const loadData = async () => {
 //         //setData(map.filePath)
-//
+
 //     };
 //     loadData();
 //   }, [filePath]);
