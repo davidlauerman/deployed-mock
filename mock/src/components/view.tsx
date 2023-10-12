b86b5e28da918f9f1d0406bba4611f123e0e10f7
davidlauerman <david_lauerman@brown.edu>
2023-10-12 18:02:20 +0200

    feat: Table object implemented

---
+++ mock/src/components/view.tsx
@@ -0,0 +1,7 @@
+import { Table } from "./Table";
+import { getData } from "./mockedJson";
+
+export function View_CSV() {
+  // return whatever file you like
+  return <Table tableData={getData("fileInt")}></Table>;
+}
