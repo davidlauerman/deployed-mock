cc966b5d569030ef7667d6882588bfed0749970e
niz <sahdiah.cox@gmail.com>
2023-10-12 02:03:08 -0400

    refactor: changed name of function to uppercase

--- mock/src/components/load.tsx
+++ mock/src/components/load.tsx
@@ -1,12 +1,13 @@
+import { Dispatch, SetStateAction } from "react";
 import { Line } from "./Line"
 
 interface loadProps {
     newFilepath: string;
     //not sure if you have to do this for things that are shared state
-    setFilepath: (filepath: string) => void;
+    setFilepath: Dispatch<SetStateAction<string>>;
 }
 
-export function load_csv(newFilepath : loadProps) {
+export function Load_CSV({newFilepath, setFilepath} : loadProps) {
     let response = 'Filepath could not be found!'
     //we need to check if we can load the data!
     //if (load_data !== false) {
