24366083d41e79a0b39b78f002e4acc065ad03a1
niz <sahdiah.cox@gmail.com>
2023-10-13 18:56:22 -0400

    feat: added tests for search and interactions

--- mock/tests/App.spec.ts
+++ mock/tests/App.spec.ts
@@ -1,55 +1,27 @@
 import { test, expect } from '@playwright/test';
 
 
-/**
-  The general shapes of tests in Playwright Test are:
-    1. Navigate to a URL
-    2. Interact with the page
-    3. Assert something about the page against your expectations
-  Look for this pattern in the tests below!
- */
-
-// If you needed to do something before every test case...
-test.beforeEach(() => {
-    // ... you'd put it here.
-    // TODO: Is there something we need to do before every test case to avoid repeating code?
-  })
-
-/**
- * Don't worry about the "async" yet. We'll cover it in more detail
- * for the next sprint. For now, just think about "await" as something 
- * you put before parts of your test that might take time to run, 
- * like any interaction with the page.
- */
 test('on page load, i see an input bar', async ({ page }) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
   await page.goto('http://localhost:8000/');
   await expect(page.getByLabel('Command input')).toBeVisible()
 })
 
 test('after I type into the input box, its text changes', async ({ page }) => {
-  // Step 1: Navigate to a URL
   await page.goto('http://localhost:8000/');
 
-  // Step 2: Interact with the page
-  // Locate the element you are looking for
   await page.getByLabel('Command input').click();
   await page.getByLabel('Command input').fill('Awesome command');
 
-  // Step 3: Assert something about the page
-  // Assertions are done by using the expect() function
   const mock_input = `Awesome command`
   await expect(page.getByLabel('Command input')).toHaveValue(mock_input)
 });
 
 test('on page load, i see a button', async ({ page }) => {
-  // TODO WITH TA: Fill this in!
   await page.goto('http://localhost:8000/');
   await expect(page.getByRole('button')).toBeVisible();
 });
 
 test('after I click the button, my command gets pushed', async ({ page }) => {
-  // TODO: Fill this in to test your button push functionality!
   await page.goto('http://localhost:8000/');
   await page.getByLabel('Command input').click();
   await page.getByLabel('Command input').fill('test');
@@ -138,4 +110,113 @@ test('when i load twice, i see the most recent in view', async ({ page }) => {
   await page.getByLabel('Command input').fill('view');
   await page.getByRole('button').click();
   await expect(page.getByText("Apple")).toBeVisible()
+});
+
+test('when i search before i load, i dont see anything', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('search');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Wrong")).toBeVisible()
+});
+
+test('search works correctly after loading', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("1")).toBeVisible()
+});
+
+test('when i load twice, i can still search', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileTxt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('search');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Apple")).toBeVisible()
+});
+
+test('when i load twice but one is wrong, i see the first one', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file ppppp');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("1")).toBeVisible()
+});
+
+test('after i view without loading(Verbose), i get bad response', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Wrong")).toBeVisible()
+  await expect(page.getByText("Output: ")).toBeVisible()
+});
+
+test('I can switch between modes multiple times', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Output: ")).toBeVisible()
+});
+
+test('when i reload i get the new search results', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('search');
+  await page.getByRole('button').click();
+  await expect(page.getByText("1")).toBeVisible()
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileTxt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('search');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Apple")).toBeVisible()
+});
+
+test('when i reload i get the new view results', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("7")).toBeVisible()
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileTxt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Elephant")).toBeVisible()
 });
