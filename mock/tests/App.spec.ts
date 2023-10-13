f40297ab83c3932f339efc371b724b1009353d4e
niz <sahdiah.cox@gmail.com>
2023-10-13 18:26:32 -0400

    feat: added tests for load, mode, and view

--- mock/tests/App.spec.ts
+++ mock/tests/App.spec.ts
@@ -44,12 +44,98 @@ test('after I type into the input box, its text changes', async ({ page }) => {
 
 test('on page load, i see a button', async ({ page }) => {
   // TODO WITH TA: Fill this in!
-});
-
-test('after I click the button, its label increments', async ({ page }) => {
-  // TODO WITH TA: Fill this in to test your button counter functionality!
+  await page.goto('http://localhost:8000/');
+  await expect(page.getByRole('button')).toBeVisible();
 });
 
 test('after I click the button, my command gets pushed', async ({ page }) => {
   // TODO: Fill this in to test your button push functionality!
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('test');
+  await page.getByRole('button').click();
+  await expect(page.getByText('Not a recognized function. Try again!')).toBeVisible()
+});
+
+test('after i push mode command, the mode changes', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await expect(page.getByText('Changed mode!')).toBeVisible()
+  //
+});
+
+test('after i push load command, i get bad response', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Filepath could not be found!")).toBeVisible()
+  // 
+});
+
+test('after i push load command with good filepath, i get good response', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await expect(page.getByText("File fileInt loaded!")).toBeVisible()
+});
+
+test('after i push load command with good filepath(Verbose), i get good response', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await expect(page.getByText("File fileInt loaded!")).toBeVisible()
+  await expect(page.getByText("Output: ")).toBeVisible()
+});
+
+test('after i push load command with bad filepath(Verbose), i get bad response', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('mode');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Filepath could not be found!")).toBeVisible()
+  await expect(page.getByText("Output: ")).toBeVisible()
+});
+
+test('when i view before i load, i dont see anything', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Wrong")).toBeVisible()
+});
+
+test('view works correctly after loading', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("1")).toBeVisible()
+});
+
+test('when i load twice, i see the most recent in view', async ({ page }) => {
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileInt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('load_file fileTxt');
+  await page.getByRole('button').click();
+  await page.getByLabel('Command input').click();
+  await page.getByLabel('Command input').fill('view');
+  await page.getByRole('button').click();
+  await expect(page.getByText("Apple")).toBeVisible()
 });
